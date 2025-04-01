521194ebea8eda954c2fe8ef56634379d52b204e
AlexAlvarezM <01alexalvarez@gmail.com>
2025-04-01 13:38:19 -0600

    Frontend_individual

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./views/Login";
+import Inicio from "./views/Inicio";
+import Encabezado from "./components/Encabezado/Encabezado";
+import Clientes from "./views/Clientes";
+import Abonos from "./views/Abonos";
+import Compras from "./views/Compras";
+import Creditos from "./views/Creditos";
+import Detalles_Compras from "./views/Detalles_Compras";
+import Detalles_Ventas from "./views/Detalles_Ventas";
+import Productos from "./views/Productos";
+import Ventas from "./views/Ventas";
+import Categorias from "./views/categoria";
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <main className = "margen-superior-main">
+      <Encabezado />
+        <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/inicio" element={<Inicio />} />
+        <Route path="/Clientes" element={<Clientes />} />
+        <Route path="/Abonos" element={<Abonos />} />
+        <Route path="/Compras" element={<Compras />} />
+        <Route path="/Creditos" element={<Creditos />} />
+        <Route path="/Detalles_Compras" element={<Detalles_Compras />} />
+        <Route path="/Detalles_Ventas" element={<Detalles_Ventas />} />
+        <Route path="/Productos" element={<Productos />} />
+        <Route path="/Ventas" element={<Ventas/>} />
+        <Route path="/categoria" element={<Categorias/>} />
+        </Routes>
+
+
+      </main>
+
+    </Router>
+  );
+};
+
+export default App;
