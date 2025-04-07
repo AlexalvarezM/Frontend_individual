ce70c504dcedd152b7bc97ae4a0355f4f1fa72f1
AlexAlvarezM <01alexalvarez@gmail.com>
2025-04-07 14:44:33 -0600

    Frontend

--- src/views/Productos.jsx
+++ src/views/Productos.jsx
@@ -1,39 +1,132 @@
+// Importaciones necesarias para la vista
 import React, { useState, useEffect } from 'react';
-import TablaProductos from '../components/productos/TablaProductos';
-import { Container } from "react-bootstrap";
+import TablaProductos from '../components/productos/TablaProductos'; // Importa el componente de tabla
+import ModalRegistroProducto from '../components/productos/ModalRegistroProducto';
+import { Container, Button } from "react-bootstrap";
 
+// Declaración del componente Productos
 const Productos = () => {
-  const [listaProductos, setListaProductos] = useState([]);
-  const [cargando, setCargando] = useState(true);
-  const [errorCarga, setErrorCarga] = useState(null);
+  // Estados para manejar los datos, carga y errores
+  const [listaProductos, setListaProductos] = useState([]); // Almacena los datos de la API
+  const [listaCategorias, setListaCategorias] = useState([]);
+  const [cargando, setCargando] = useState(true);            // Controla el estado de carga
+  const [errorCarga, setErrorCarga] = useState(null);        // Maneja errores de la petición
+  const [mostrarModal, setMostrarModal] = useState(false);
+  const [nuevoProducto, setNuevoProducto] = useState({
+    nombre_producto: '',
+    descripcion: '',
+    categoria: '',
+    precio_unitario: '',
+    stock: ''
+  });
 
-  useEffect(() => {
-    const obtenerProductos = async () => {
-      try {
-        const respuesta = await fetch('http://localhost:3000/api/productos');
-        if (!respuesta.ok) throw new Error('Error al cargar los productos');
-        const datos = await respuesta.json();
-        setListaProductos(datos);
-        setCargando(false);
-      } catch (error) {
-        setErrorCarga(error.message);
-        setCargando(false);
+  const obtenerProductos = async () => { // Método renombrado a español
+    try {
+      const respuesta = await fetch('http://localhost:3000/api/clientes');
+      if (!respuesta.ok) {
+        throw new Error('Error al cargar los productos');
       }
-    };
-    obtenerProductos();
-  }, []);
+      const datos = await respuesta.json();
+      setListaProductos(datos);    // Actualiza el estado con los datos
+      setCargando(false);           // Indica que la carga terminó
+    } catch (error) {
+      setErrorCarga(error.message); // Guarda el mensaje de error
+      setCargando(false);           // Termina la carga aunque haya error
+    }
+  };
 
+  // Obtener clientes para el dropdown
+  const obtenerCategorias = async () => {
+    try {
+      const respuesta = await fetch('http://localhost:3000/api/categorias');
+      if (!respuesta.ok) throw new Error('Error al cargar los Clientes');
+      const datos = await respuesta.json();
+      setListaCategorias(datos);
+    } catch (error) {
+      setErrorCarga(error.message);
+    }
+  };
+
+  // Lógica de obtención de datos con useEffect
+  useEffect(() => {
+    obtenerProductos();  
+    obtenerCategorias();          // Ejecuta la función al montar el componente
+  }, []);                           // Array vacío para que solo se ejecute una vez
+
+
+// Maneja los cambios en los inputs del modal
+const manejarCambioInput = (e) => {
+  const { name, value } = e.target;
+  setNuevoProducto(prev => ({
+    ...prev,
+    [name]: value
+  }));
+};
+
+// Manejo la inserción de un nuevo producto
+const agregarProducto = async () => {
+
+  if (!nuevoProducto.nombre_producto|| !nuevoProducto.descripcion) {
+  setErrorCarga("Por favor, completa todos los campos antes de guardar.");
+  return;
+  }
+
+  try {
+    const respuesta = await fetch('http://localhost:3000/api/registrarcategoria', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(nuevoProducto),
+    });
+
+    if (!respuesta.ok) {
+      throw new Error('Error al agregar el producto');
+    }
+
+    await obtenerProductos(); // Refresca toda la lista desde el servidor
+    setNuevoCliente({ nombre_producto: '', descripcion: '' });
+    setMostrarModal(false);
+    setErrorCarga(null);
+  } catch (error) {
+    setErrorCarga(error.message);
+  }
+};
+
+  // Renderizado de la vista
   return (
-    <Container className="mt-5">
-      <br />
-      <h4>Productos</h4>
-      <TablaProductos
-        productos={listaProductos}
-        cargando={cargando}
-        error={errorCarga}
-      />
-    </Container>
+    <>
+      <Container className="mt-5">
+        <br />
+        <h4>Productos</h4>
+        <Button variant="primary" onClick={() => setMostrarModal(true)}>
+          Nuevo Producto
+        </Button>
+        <br/><br/>
+
+        {/* Pasa los estados como props al componente TablaProductos */}
+        <TablaProductos
+          productos={listaProductos} 
+          cargando={cargando} 
+          error={errorCarga} 
+        />
+
+        <ModalRegistroProducto
+          mostrarModal={mostrarModal}
+          setMostrarModal={setMostrarModal}
+          nuevoProducto={nuevoProducto}
+          manejarCambioInput={manejarCambioInput}
+          agregarProducto={agregarProducto}
+          errorCarga={errorCarga}
+          categoria={listaCategorias}
+        />
+        
+      </Container>
+    </>
+    
   );
 };
 
+
+// Exportación del componente
 export default Productos;
--- src/views/categoria.jsx
+++ src/views/categoria.jsx
@@ -1,8 +1,10 @@
 // Importaciones necesarias para la vista
 import React, { useState, useEffect } from 'react';
 import TablaCategorias from '../components/categorias/TablaCategorias'; // Importa el componente de tabla
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Row, Col } from "react-bootstrap";
 import ModalRegistroCategoria from '../components/categorias/ModalRegistroCategoria';
+import CuadroBusquedas from '../components/busquedas/CuadroBusquedas';
+
 
 
 // Declaración del componente Categorias
@@ -17,6 +19,9 @@ const Categorias = () => {
     descripcion_categoria: ''
   });
 
+  const [categoriasFiltradas, setCategoriasFiltradas] = useState([]);
+  const [textoBusqueda, setTextoBusqueda] = useState("");
+
 
   const obtenerCategorias = async () => { // Método renombrado a español
     try {
@@ -26,6 +31,7 @@ const Categorias = () => {
       }
       const datos = await respuesta.json();
       setListaCategorias(datos);    // Actualiza el estado con los datos
+      setCategoriasFiltradas(datos);
       setCargando(false);           // Indica que la carga terminó
     } catch (error) {
       setErrorCarga(error.message); // Guarda el mensaje de error
@@ -80,7 +86,17 @@ const Categorias = () => {
     }
   };
 
-
+  const manejarCambioBusqueda = (e) => {
+    const texto = e.target.value.toLowerCase();
+    setTextoBusqueda(texto);
+    
+    const filtradas = listaCategorias.filter(
+      (categoria) =>
+        categoria.nombre_categoria.toLowerCase().includes(texto) ||
+        categoria.descripcion_categoria.toLowerCase().includes(texto)
+    );
+    setCategoriasFiltradas(filtradas);
+  };
 
   // Renderizado de la vista
   return (
@@ -89,14 +105,26 @@ const Categorias = () => {
         <br />
         <h4>Categorías</h4>
 
-        <Button variant="primary" onClick={() => setMostrarModal(true)}>
-          Nueva Categoría
-        </Button>
+        <Row>
+        <Col lg={2} md={4} sm={4} xs={5}>
+      <Button variant="primary" onClick={() => setMostrarModal(true)} style={{ width: "100%" }}>
+        Nueva Categoría
+      </Button>
+      </Col>
+      <Col lg={5} md={8} sm={8} xs={7}>
+      <CuadroBusquedas
+        textoBusqueda={textoBusqueda}
+        manejarCambioBusqueda={manejarCambioBusqueda}
+       />
+        </Col>
+        </Row>
+
+
         <br/><br/>
 
         {/* Pasa los estados como props al componente TablaCategorias */}
         <TablaCategorias 
-          categorias={listaCategorias} 
+          categorias={categoriasFiltradas} 
           cargando={cargando} 
           error={errorCarga} 
         />
